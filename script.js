ec5810d22126b8ce10415c8a935deaf7560b678d
GHSRobert Ciang <Robertus0617@gmail.com>
2017-10-14 15:00:55 +0800

    Replacing deprecated jQuery.browser
    Thanks to https://stackoverflow.com/questions/9645803/whats-the-replacement-for-browser#answer-15072522

--- script.js
+++ script.js
@@ -4,6 +4,34 @@
  * @author    Tom N Harris <tnharris@whoopdedo.org>
  */
 jQuery(function (){
+  jQuery.uaMatch = function( ua ) {
+    ua = ua.toLowerCase();
+    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) || [];
+    return {
+        browser: match[ 1 ] || "",
+        version: match[ 2 ] || "0"
+    };
+};
+if ( !jQuery.browser ) {
+    var 
+    matched = jQuery.uaMatch( navigator.userAgent ),
+    browser = {};
+    if ( matched.browser ) {
+        browser[ matched.browser ] = true;
+        browser.version = matched.version;
+    }
+    // Chrome is Webkit, but Webkit is also Safari.
+    if ( browser.chrome ) {
+        browser.webkit = true;
+    } else if ( browser.webkit ) {
+        browser.safari = true;
+    }
+    jQuery.browser = browser;
+}
   if(jQuery.browser.msie && 8 > parseFloat(jQuery.browser.version)){
     // Does MSIE support :before and :after ?
     function isParentNode(p, n){
